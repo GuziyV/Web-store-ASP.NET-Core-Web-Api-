480d3136d755dbc84c3896343e2085f184778e59
Volodymyr <35489438+GuziyV@users.noreply.github.com>
2019-03-20 18:09:52 +0200

    Create Product in Admin controller

---
+++ BL/Services/AdminService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BL.DTOs;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BL.Services {
+	public class AdminService { 
+		protected readonly DbContext context;
+		protected readonly IMapper mapper;
+
+		public AdminService(DbContext context, IMapper mapper) {
+			this.mapper = mapper;
+			this.context = context;
+		}
+
+		public async Task<ProducerDTO> AddProduct(ProductDTO product) {
+			var resEntity = context != null ?
+				(await context.AddAsync<Producer>(mapper.Map<Producer>(product))).Entity :
+				null;
+			await context.SaveChangesAsync();
+			return mapper.Map<ProducerDTO>(resEntity);
+		}
+	}
+}
--- WebStore-API/Controllers/AdminController.cs
+++ WebStore-API/Controllers/AdminController.cs
@@ -2,45 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BL.DTOs;
+using BL.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebStore_API.Controllers
-{
+namespace WebStore_API.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class AdminController : ControllerBase
-    {
-        // GET: api/Admin
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+    public class AdminController : ControllerBase {
+	    private AdminService adminService;
 
-        // GET: api/Admin/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
+	    public AdminController(AdminService adminService) {
+		    this.adminService = adminService;
+	    }
         // POST: api/Admin
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Admin/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        [HttpPost("addProduct")]
+        public async Task<ProducerDTO> Post([FromBody] ProductDTO product) {
+	        return await adminService.AddProduct(product: product);
         }
     }
 }
